f9e9df6315edcab2fd267f7280e83c6962534501
Luca Perico <pericoluca1993@gmail.com>
2014-11-28 11:23:32 +0100

    Aggiornato InsertBoat
    Implementato controllo su tipo di dati lunghezza e prezzo barca.

--- LoginEsercizio/LoginEsercizio/InsertBoat.cs
+++ LoginEsercizio/LoginEsercizio/InsertBoat.cs
@@ -18,11 +18,28 @@ namespace LoginEsercizio
 
         private void button1_Click(object sender, EventArgs e)
         {
+            //controllo tipo dati
+            float n_lunghezza;
+            bool isNumeric = float.TryParse(this.textBoxLunghezza.Text, out n_lunghezza);
+            if (!isNumeric)
+            {
+                MessageBox.Show("La lunghezza deve essere un numero");
+                return;
+            }
+
+            float n_prezzo;
+            isNumeric = float.TryParse(this.textBoxPrezzo.Text, out n_prezzo);
+            if (!isNumeric)
+            {
+                MessageBox.Show("Il prezzo deve essere un numero");
+                return;
+            }
+
             Sailboat s = new Sailboat();
             s.IDUnivoco = FormPrincipale.barcheInserite.Count();
             s.marca = this.textBoxMarca.Text;
             s.nome = this.textBoxNome.Text;
-            s.lunghezza = float.Parse(this.textBoxLunghezza.Text);
+            s.lunghezza = n_lunghezza;
             s.prezzo = float.Parse(this.textBoxPrezzo.Text);
 
             FormPrincipale.barcheInserite.Add(s);
