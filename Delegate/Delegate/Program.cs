43866ee3a404f044bc0f629567fbe6cdee67fce4
Luca Perico <pericoluca1993@gmail.com>
2014-12-02 13:47:46 +0100

    Revert 3a11564..6c1a2b4
    This rolls back to commit 3a115648f75c95067bc95492be9997df2192fe40.

--- Delegate/Delegate/Program.cs
+++ Delegate/Delegate/Program.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -45,51 +44,3 @@ namespace Delegate
         }
     }
 }
-=======
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-delegate void Op(double n1, double n2);
-
-namespace Delegate
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            MathOpClass m = new MathOpClass();
-            m.Operazione += new Op(MathOpClass.Add);
-            m.Operazione += new Op(MathOpClass.Multiply);
-            m.Operazione += new Op(MathOpClass.Sub);
-            m.CallOp(2, 3);
-        }
-    }
-
-    class MathOpClass
-    {
-        public Op Operazione;
-  
-        public static void Add(double n1, double n2)
-        {
-            Console.WriteLine((n1+n2) + " = " + n1 + " + " + n2);
-        }
-
-        public static void Multiply(double n1, double n2)
-        {
-            Console.WriteLine((n1 * n2) + " = " + n1 + " * " + n2);
-        }
-
-        public static void Sub(double n1, double n2)
-        {
-            Console.WriteLine((n1 - n2) + " = " + n1 + " - " + n2);
-        }
-
-        public  void CallOp(double n1, double n2)
-        {
-            Operazione(n1, n2);   
-        }
-    }
-}
->>>>>>> origin/master
--- Delegate/Delegate/Properties/AssemblyInfo.cs
+++ Delegate/Delegate/Properties/AssemblyInfo.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
@@ -35,41 +34,3 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-=======
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-// Le informazioni generali relative a un assembly sono controllate dal seguente 
-// set di attributi. Per modificare le informazioni associate a un assembly
-// occorre quindi modificare i valori di questi attributi.
-[assembly: AssemblyTitle("Delegate")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Delegate")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Se si imposta ComVisible su false, i tipi in questo assembly non saranno visibili 
-// ai componenti COM. Se è necessario accedere a un tipo in questo assembly da 
-// COM, impostare su true l'attributo ComVisible per tale tipo.
-[assembly: ComVisible(false)]
-
-// Se il progetto viene esposto a COM, il GUID che segue verrà utilizzato per creare l'ID della libreria dei tipi
-[assembly: Guid("41c88c8b-3f68-4d5c-838f-caa76b9fb17a")]
-
-// Le informazioni sulla versione di un assembly sono costituite dai seguenti quattro valori:
-//
-//      Numero di versione principale
-//      Numero di versione secondario 
-//      Numero build
-//      Revisione
-//
-// È possibile specificare tutti i valori oppure impostare valori predefiniti per i numeri relativi alla revisione e alla build 
-// utilizzando l'asterisco (*) come descritto di seguito:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
->>>>>>> origin/master
