82307735844ed0bb4e86611263530728d5adff47
Luca Perico <pericoluca1993@gmail.com>
2014-12-03 15:39:13 +0100

    Aggiunto esercizio sugli eventi

---
+++ EventCounter/EventCounter/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace EventCounter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Liberare le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.labelValoreCount = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // labelValoreCount
+            // 
+            this.labelValoreCount.AutoSize = true;
+            this.labelValoreCount.Location = new System.Drawing.Point(180, 19);
+            this.labelValoreCount.Name = "labelValoreCount";
+            this.labelValoreCount.Size = new System.Drawing.Size(35, 13);
+            this.labelValoreCount.TabIndex = 1;
+            this.labelValoreCount.Text = "label1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 48);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Run";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(13, 94);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(99, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Rimuovi handler";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.labelValoreCount);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label labelValoreCount;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ EventCounter/EventCounter/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using EventCounter;
+
+namespace EventCounter
+{
+    public partial class Form1 : Form
+    {
+        int _X;
+        public static int reachableNum;
+        Counter c = null;
+        public Form1()
+        {
+            InitializeComponent();
+            c = new Counter();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            _X = int.Parse(this.textBox1.Text);
+            if (_X > 100000)
+            {
+                MessageBox.Show("Il numero deve essere minore di 100.000");
+                return;
+            }
+            Random random = new Random();
+            int Y = random.Next(0, 10000);
+            reachableNum = _X + Y;
+            this.labelValoreCount.Text = "" + (reachableNum / 100) + " secondi!";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            c.CountTo();
+        }
+
+        public static void oCounter_NumberReached(object sender, NumberReachedHandlerEventArgs e)
+        {
+            MessageBox.Show(e.GetMessage + " " + reachableNum);
+        }
+
+        public static void oCounter_NumberReached2(object sender, NumberReachedHandlerEventArgs e)
+        {
+            MessageBox.Show(e.GetMessage + " " + reachableNum);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            c.NumberReached -= new Counter.NumberReachedHandler(Form1.oCounter_NumberReached2);
+        }
+    }
+
+    public class Counter
+    {
+        public delegate void NumberReachedHandler(object sender, NumberReachedHandlerEventArgs e);
+        public event NumberReachedHandler NumberReached;
+
+        public Counter()
+        {
+            NumberReached += new NumberReachedHandler(Form1.oCounter_NumberReached2) + new NumberReachedHandler(Form1.oCounter_NumberReached);
+        }
+
+        public void CountTo()
+        {
+            for (int i = 0; i < Form1.reachableNum; ++i)
+            {
+                System.Threading.Thread.Sleep(10);
+            }
+            NumberReached(this, new NumberReachedHandlerEventArgs("Numero raggiunto!"));
+        }
+    }
+
+    public class NumberReachedHandlerEventArgs : EventArgs
+    {
+        protected string _message;
+        public NumberReachedHandlerEventArgs(string message)
+        {
+            _message = message;
+        }
+
+        public string GetMessage
+        {
+            get { return _message; }
+        }
+    }
+}
