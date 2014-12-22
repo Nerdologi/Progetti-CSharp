6c95d86e1e946c85358a081951caaf470d10dcfb
SilvioMessi <messisilvio@gmail.com>
2014-12-22 11:18:43 +0100

    Creazione progetto, realizzazione socket e parsing
    Ho creato un nuovo progetto "Esame". Il codice apre una socket in
    ascolto sulla porta 455555 e quando il client si connette viene letto
    e analizzato lo stream della socket per estrapolare i dati che il client
    ci fornisce.

---
+++ Esame/Esame/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace Esame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Liberare le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.startServer = new System.Windows.Forms.Button();
+            this.textBoxInfo = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // startServer
+            // 
+            this.startServer.Location = new System.Drawing.Point(77, 226);
+            this.startServer.Name = "startServer";
+            this.startServer.Size = new System.Drawing.Size(120, 23);
+            this.startServer.TabIndex = 0;
+            this.startServer.Text = "START SERVER";
+            this.startServer.UseVisualStyleBackColor = true;
+            this.startServer.Click += new System.EventHandler(this.startServer_Click);
+            // 
+            // textBoxInfo
+            // 
+            this.textBoxInfo.Location = new System.Drawing.Point(13, 13);
+            this.textBoxInfo.Multiline = true;
+            this.textBoxInfo.Name = "textBoxInfo";
+            this.textBoxInfo.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxInfo.Size = new System.Drawing.Size(259, 207);
+            this.textBoxInfo.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.textBoxInfo);
+            this.Controls.Add(this.startServer);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startServer;
+        private System.Windows.Forms.TextBox textBoxInfo;
+    }
+}
+
---
+++ Esame/Esame/Form1.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Esame
+{
+    public partial class Form1 : Form
+    {
+        public static TextBox info;
+        public Form1()
+        {
+            InitializeComponent();
+            info = this.textBoxInfo;
+        }
+
+        private void startServer_Click(object sender, EventArgs e)
+        {
+            Server.StartListening();
+        }
+    }
+    public class Server
+    {
+        public static void StartListening()
+        {
+            // Establish the local endpoint for the socket.
+            // Dns.GetHostName returns the name of the 
+            // host running the application.
+            IPHostEntry ipHostInfo = Dns.Resolve(Dns.GetHostName());
+            IPAddress ipAddress = ipHostInfo.AddressList[0];
+            IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 45555);
+
+            // Create a TCP/IP socket.
+            Socket listener = new Socket(AddressFamily.InterNetwork,
+                SocketType.Stream, ProtocolType.Tcp);
+
+            // Bind the socket to the local endpoint and 
+            // listen for incoming connections.
+            try
+            {
+                listener.Bind(localEndPoint);
+                listener.Listen(10);
+
+                // Start listening for connections.
+                while (true)
+                {
+                    Form1.info.AppendText("HOST IP: " + ipAddress.ToString()  + "\r\nWaiting for a connection....\r\n");
+                    // Program is suspended while waiting for an incoming connection.
+                    Socket handler = listener.Accept();
+                    NetworkStream myNetworkStream =  new NetworkStream(handler, true);
+                    BinaryReader bin = new BinaryReader(myNetworkStream);
+                    int byteToRead;
+                    byte[] pacchetto;
+                    int numSensori;
+                    int maxSensori = 10;
+                    byte[] len = new byte[2];
+                    byte[] tem = new byte[3];
+                    while (!(tem[0] == 0xFF && tem[1] == 0x32)) // cerca la sequenza FF-32
+                    {
+                        tem[0] = tem[1];
+                        tem[1] = tem[2];
+                        byte[] read = bin.ReadBytes(1);
+                        tem[2] = read[0];
+                    }
+                    if (tem[2] != 0xFF) // modalità normale
+                    {
+                        byteToRead = tem[2]; // byte da leggere
+                    }
+                    else  // modalità extended-length
+                    {
+                        len = new byte[2];
+                        len = bin.ReadBytes(2);
+                        byteToRead = (len[0] * 256) + len[1]; // byte da leggere
+                    }
+
+                    byte[] data = new byte[byteToRead + 1];
+                    data = bin.ReadBytes(byteToRead + 1); // lettura dei dati
+
+                    if (tem[2] != 0xFF)
+                    {
+                        pacchetto = new byte[byteToRead + 4]; // creazione pacchetto
+                    }
+                    else
+                    {
+                        pacchetto = new byte[byteToRead + 6];
+                    }
+
+                    numSensori = (byteToRead - 2) / 52; // calcolo del numero di sensori
+                    pacchetto[0] = 0xFF; // copia dei primi elementi
+                    pacchetto[1] = 0x32;
+                    pacchetto[2] = tem[2];
+
+                    if (tem[2] != 0xFF)
+                    {
+                        data.CopyTo(pacchetto, 3); // copia dei dati
+                    }
+                    else
+                    {
+                        pacchetto[3] = len[0];
+                        pacchetto[4] = len[1];
+                        data.CopyTo(pacchetto, 5); // copia dei dati
+                    }
+
+
+                    List<List<double>> array = new List<List<double>>(); // salvataggio dati
+
+
+                    int[] t = new int[maxSensori];
+
+                    for (int x = 0; x < numSensori; x++)
+                    {
+                        array.Add(new List<double>()); // una lista per ogni sensore
+                        t[x] = 5 + (52 * x);
+                    }
+                    bool part1 = handler.Poll(1000, SelectMode.SelectRead);
+                    bool part2 = (handler.Available == 0);
+                    while (!part1 || !part2)
+                    //while(true) crash
+                    {
+                        for (int i = 0; i < numSensori; i++)
+                        {
+                            byte[] temp = new byte[4];
+                            for (int tr = 0; tr < 13; tr++)// 13 campi, 3 * 3 + 4
+                            {
+                                if (numSensori < 5)
+                                {
+                                    temp[0] = pacchetto[t[i] + 3]; // lettura inversa
+                                    temp[1] = pacchetto[t[i] + 2];
+                                    temp[2] = pacchetto[t[i] + 1];
+                                    temp[3] = pacchetto[t[i]];
+                                }
+                                else
+                                {
+                                    temp[0] = pacchetto[t[i] + 5];
+                                    temp[1] = pacchetto[t[i] + 4];
+                                    temp[2] = pacchetto[t[i] + 3];
+                                    temp[3] = pacchetto[t[i] + 2];
+                                }
+                                float valore = BitConverter.ToSingle(temp, 0); // conversione
+                                array[i].Add(valore); // memorizzazione
+                                t[i] += 4;
+                            }
+                        }
+                        for (int x = 0; x < numSensori; x++)
+                        {
+                            t[x] = 5 + (52 * x);
+                        }
+
+                        for (int j = 0; j < numSensori; j++)
+                        {
+                            for (int tr = 0; tr < 13; tr++)
+                            {
+                                Form1.info.AppendText(array[j][tr] + "; ");
+                            }
+                            array[j].RemoveRange(0, 13); // cancellazione dati
+                        }
+                        if (numSensori < 5) // lettura pacchetto seguente
+                        {
+                            pacchetto = bin.ReadBytes(byteToRead + 4);
+                        }
+                        else
+                        {
+                            pacchetto = bin.ReadBytes(byteToRead + 6);
+                        }
+                        part1 = handler.Poll(1000, SelectMode.SelectRead);
+                        part2 = (handler.Available == 0);
+                    }
+
+                    handler.Shutdown(SocketShutdown.Both);
+                    handler.Close();   
+                    
+                }
+
+            }
+            catch (Exception e)
+            {
+                Form1.info.AppendText(e.ToString());
+            }
+
+        }
+    
+    }
+}
