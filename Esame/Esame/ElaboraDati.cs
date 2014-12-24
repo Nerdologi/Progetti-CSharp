abf9df6f5e5d0e9ca8378d4a2e51f1846797a10b
Luca Perico <pericoluca1993@gmail.com>
2014-12-24 15:40:21 +0100

    Aggiornato codice dopo conferenza del 24-12  - sistemata la funzione modulation  - aggiunte scritte su schermo relative a modulatione e smoothing

--- Esame/Esame/ElaboraDati.cs
+++ Esame/Esame/ElaboraDati.cs
@@ -7,70 +7,47 @@ namespace Esame
 {
     class ElaboraDati
     {
-       
-        public static List<AngoloEulero[]> angoliEulero(List<float[,]> campioni)
-        {
-
-            List<AngoloEulero[]> angoliEulero = new List<AngoloEulero[]>();
-            for (int i = 0; i < campioni.Count; i++)
-            {
-                //un angolo di eulero per ogni sensore (nel nostro caso sono 5)
-                //nel caso di un numero di sensori variabile bisogna pescare il numero di
-                //sensori dalla classe Server
-                angoliEulero.Add(new AngoloEulero[5]);
-                //itero i sensori del campione che sto considerando
-                for (int numSensore = 0; numSensore < 5; numSensore++)
-                {
-                    float q0 = campioni[i][numSensore, 9];
-                    float q1 = campioni[i][numSensore, 10];
-                    float q2 = campioni[i][numSensore, 11];
-                    float q3 = campioni[i][numSensore, 12];
-                    float yaw = (float)Math.Atan(((2 * q2 * q3) + (2 * q0 * q1)) / ((2 * q0 * q0) + (2 * q3 * q3) - 1));
-                    float pitch = (float)Math.Asin(((2 * q1 * q3) - (2 * q0 * q2)));
-                    float roll = (float)Math.Atan(((2 * q1 * q2) + (2 * q0 * q3)) / ((2 * q0 * q0) + (2 * q1 * q1) - 1));
-                    angoliEulero[i][numSensore] = new AngoloEulero(yaw, pitch, roll);
-                }
-            }
-
-            return angoliEulero;
-
-        }
-        public static List<float> modulation(List<float[,]> samples , int _type)
+        /*
+         * modulation accetta come parametri i campioni, il numero del sensore (0..5), e 
+         * il tipo di modulazione da eseguire: 
+         *  - 0 per modulare l'accelerometro
+         *  - 1 per modulare il giroscopio
+         * ritorna un array monodimensionale
+         * */
+        public static List<float> modulation(List<float[,]> samples, int _S, int _type)
         {
             float a0, a1, a2;
             float g0, g1, g2;
             List<float> valori = new List<float>();
             for (int i = 0; i < samples.Count; i++)
             {
-                for (int numSensore = 0; numSensore < 5; numSensore++)
+                if (_type == 0)
                 {
-                    if (_type == 0)
-                    {
-                        a0 = samples[i][numSensore, 0];
-                        a1 = samples[i][numSensore, 1];
-                        a2 = samples[i][numSensore, 2];
-                        a0 = (float)Math.Pow(a0, 2);
-                        a1 = (float)Math.Pow(a1, 2);
-                        a2 = (float)Math.Pow(a2, 2);
-                        valori[i] = (float)Math.Sqrt((double)a0 + (double)a1 + (double)a2);
+                    a0 = samples[i][_S, 0];
+                    a1 = samples[i][_S, 1];
+                    a2 = samples[i][_S, 2];
+                    a0 = (float)Math.Pow(a0, 2);
+                    a1 = (float)Math.Pow(a1, 2);
+                    a2 = (float)Math.Pow(a2, 2);
+                    valori.Add((float)Math.Sqrt(a0 + a1 + a2));
                         
-                    }
-                    else
-                    {
-                        g0 = samples[i][numSensore, 0];
-                        g1 = samples[i][numSensore, 1];
-                        g2 = samples[i][numSensore, 2];
-                        g0 = (float)Math.Pow(g0, 2);
-                        g1 = (float)Math.Pow(g1, 2);
-                        g2 = (float)Math.Pow(g2, 2);
-                        valori[i] = (float)Math.Sqrt(g0 + g1 + g2);
+                }
+                else
+                {
+                    g0 = samples[i][_S, 3];
+                    g1 = samples[i][_S, 4];
+                    g2 = samples[i][_S, 5];
+                    g0 = (float)Math.Pow(g0, 2);
+                    g1 = (float)Math.Pow(g1, 2);
+                    g2 = (float)Math.Pow(g2, 2);
+                    valori.Add((float)Math.Sqrt(g0 + g1 + g2));
                     
-                    }
                 }
             }
 
             return valori;         
         }
+
         public static List<float> smoothing(List<float> module)
         {
             List<float> valori = new List<float>();
@@ -115,8 +92,7 @@ namespace Esame
                      h = i + 1;
                     while (f < K)
                     {
-                        
-                        summary += module[j];
+                        summary += module[j]; //crasha con j=-1
                         summary += module[h];
                         K++;
                         j--;
@@ -126,12 +102,36 @@ namespace Esame
 
                   }
                 //FINE CASISTICA GENERALE
-               /* ////////////
-                TODO ALTRE CASISTICHE
-                *//////////////
                 valori[i] = mean;
             }
             return valori;
+        }
+
+        public static List<AngoloEulero[]> angoliEulero(List<float[,]> campioni)
+        {
+
+            List<AngoloEulero[]> angoliEulero = new List<AngoloEulero[]>();
+            for (int i = 0; i < campioni.Count; i++)
+            {
+                //un angolo di eulero per ogni sensore (nel nostro caso sono 5)
+                //nel caso di un numero di sensori variabile bisogna pescare il numero di
+                //sensori dalla classe Server
+                angoliEulero.Add(new AngoloEulero[5]);
+                //itero i sensori del campione che sto considerando
+                for (int numSensore = 0; numSensore < 5; numSensore++)
+                {
+                    float q0 = campioni[i][numSensore, 9];
+                    float q1 = campioni[i][numSensore, 10];
+                    float q2 = campioni[i][numSensore, 11];
+                    float q3 = campioni[i][numSensore, 12];
+                    float yaw = (float)Math.Atan(((2 * q2 * q3) + (2 * q0 * q1)) / ((2 * q0 * q0) + (2 * q3 * q3) - 1));
+                    float pitch = (float)Math.Asin(((2 * q1 * q3) - (2 * q0 * q2)));
+                    float roll = (float)Math.Atan(((2 * q1 * q2) + (2 * q0 * q3)) / ((2 * q0 * q0) + (2 * q1 * q1) - 1));
+                    angoliEulero[i][numSensore] = new AngoloEulero(yaw, pitch, roll);
+                }
+            }
+
+            return angoliEulero;
         }
     }
 }
